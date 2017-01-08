4c38fe09b4514d9b7f38f95048e5f58873522474
chegola <arkhomk@yahoo.com>
2017-01-08 11:50:16 +0700

    Able to bouncing maker when mouse over

--- src/main/webapp/app/entities/shop/shop.controller.js
+++ src/main/webapp/app/entities/shop/shop.controller.js
@@ -11,7 +11,20 @@
         var vm = this;
 
         vm.shops = [];
+        vm.geom = [];
 
+        vm.do_mouseover = do_mouseover;
+        vm.do_mouseleave = do_mouseleave;
+
+        function do_mouseleave(key) {
+            console.debug("Enter ng-mouseleave: " + key);
+            vm.geom[key].animation = "Animation.STOP";
+        }
+
+        function do_mouseover(key) {
+            console.debug("Enter ng-mouseover:" + key);
+            vm.geom[key].animation = "Animation.BOUNCE";
+        }
 
         if ($state.current.name === 'shop.findNearBy') {
             loadByFindNearBy();
@@ -39,7 +52,23 @@
             Shop.query(function(result) {
                 vm.shops = result;
                 vm.searchQuery = null;
+                populateResult();
             });
+        }
+
+        function populateResult() {
+            var arrayLength = vm.shops.length;
+            for(var i=0; i < arrayLength; i++) {
+                var obj = new Object();
+                var shop = vm.shops[i];
+                obj.key = i;
+                obj.id = shop.id;
+                obj.name = shop.name;
+                obj.lat = shop.location.coordinates[0];
+                obj.lon = shop.location.coordinates[1];
+                obj.animation = "Animation.STOP";
+                vm.geom[i] = obj;
+            }
         }
     }
 })();
